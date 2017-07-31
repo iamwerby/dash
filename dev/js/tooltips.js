0a1c7dfcd590f6cf3f0823a150861b4488b4ad2a
Perejro <krulkevych@gmail.com>
2017-07-31 20:26:01 +0300

    Добавил туллтипы

---
+++ dev/js/tooltips.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$('.hex').mouseover(function(){
+		$(".hex").removeClass('active');
+  		$(this).addClass('active');
+	});
+
+	$('.hex').mouseout(function(event){
+		$(".hex").removeClass('active');
+	});
+})
